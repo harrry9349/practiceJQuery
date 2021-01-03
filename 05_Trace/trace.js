38468596d5ab7b8bb201aaa9f8a5755ad49f9086
harrry9349 <harrry9349@gmail.com>
2021-01-03 20:04:35 +0900

    1

---
+++ 05_Trace/trace.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+
+    // 上から2番目のpに背景色赤：文字色白を設定
+    $('.main1 button').on('click',function(){
+        $('.sticky').eq(2).css('backgroundColor','red').css('color','white');
+    });
+
+    // 下から3番目のpに背景色赤：文字色白を設定
+    $('.main2 button').on('click',function(){
+        $('.sticky').eq(-3).css('backgroundColor','red').css('color','white');
+    });
+    
+    // セレクタで除外
+    $('#button1').on('click',function(){
+        $('.sticky3').not(':nth-child(3)').css('backgroundColor','red');
+    });
+
+    // クロージャで除外
+    $('#button2').on('click',function(){
+        $('.sticky3').not(function(){
+            if($(this).index() == 1){
+                return $(this);
+            }
+        }).css('backgroundColor','red');
+    });
+        
+    // ネコ族以外
+    $('.main4 button').on('click',function(){
+        $('li > div').has('.notcat').css('backgroundColor','red').css('color','white');
+    });
+    
+
+
+
+});
