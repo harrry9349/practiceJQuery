c3ba843066c186f372d5b0fcbdd02da4bb0458c2
harrry9349 <harrry9349@gmail.com>
2021-02-28 18:54:43 +0900

    1

--- 13_Anime/anime.js
+++ 13_Anime/anime.js
@@ -16,4 +16,34 @@ $(document).ready(function(){
     $("#back").on('click',function(){
         $(".main2 img").animate({left: "+=50px"},"slow");
     });
+
+
+    $("#move").on('click',function(){
+        $(".main3 img").animate({left: "+=800px"},2000);
+    });
+
+    $("#stop").on('click',function(){
+        $(".main3 img").stop();
+    });
+
+    $("#return").on('click',function(){
+        $(".main3 img").animate({left: "-=400px"},2000);
+    });
+
+    $("#start").on('click',function(e){
+        $(".area").css("backgroundColor","blue");
+        $(".area").animate({left: "+=200px"},1500);
+        $(".area").delay(1000);
+        $(".area").animate({top: "+=100px"},400);
+        $(".area").delay(3000);
+
+        $(".area").queue(function(){
+            $(".area").css("backgroundColor","red");
+            $(".area").dequeue();
+        });
+    });
+
+    $("#clear").on('click',function(){
+        $(".area").clearQueue();
+    });
 });
